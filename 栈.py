2c950bd2c74c24f27060dd6e16bd285993d2d004
afraquick <34334481+afraquick@users.noreply.github.com>
2018-06-28 15:28:11 +0800

    Add files via upload

---
+++ 栈.py
@@ -0,0 +1,11 @@
+def greetname(name):
+    print("hello,"+name+"!")
+    greet2(name)
+    bye()
+
+def greet2(name):
+    print("How are you,"+name)
+def bye():
+    print("bye my friends!")
+
+greetname("anna")
